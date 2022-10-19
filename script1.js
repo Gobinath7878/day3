1c3f906f69f15b59e4b3f035ec5178859e158ba6
Gobinath S <115769414+Gobinath7878@users.noreply.github.com>
2022-10-19 21:00:36 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,25 @@
+// for loop itration in JSON
+var numbers = [ 10, '20', 30, 'India', 50] 
+for (var i=0; i < numbers.length; i++) {
+   console.log(numbers[i])
+}
+
+// for in loop
+var person = {
+    firstname: "Virat ",
+    lastname: "Kohli",
+    age: 33,
+    team:"India"
+}; 
+ for (let z in person) {
+    console.log(z + ": "+ person[z])
+ }
+
+// for of loop
+
+var code=["HTML","CSS","JAVASCRIPT","C","C++"]
+for(var index of code){
+    console.log(index)
+}
+
+
---
+++ script2.js
@@ -0,0 +1,38 @@
+var resume=[{
+    "Name":"Gobinath S",
+},
+{
+    "PG Qualification":"M.Sc",
+    "PG Percentage":"79%",
+    "PG college":"AAGAC",
+    "PG-Year of Passout":"2022",
+    "UG Qualification":"B.Sc",
+    "UG Percentage":"76%",
+    "UG college":"GASC",
+    "UG-Year of Passout":"2020"
+},
+{
+    "Achivement":"Kamarajar Award of Tamil Nadu-2017",
+    "Technical Skills":["HTML","CSS","JAVASCRIPT"],
+    "Area of Interest":"Front-End Developer",
+    "Soft Skills":["Quick Learner","Puntual","Perseverance","Honest"],
+    "Hobbies":["Playing Games","Reading Books","Listening Songs"]
+},
+{
+    "Project":["Alternative Fuel-Hydrogen and its Thermodynamic Behaviour","Ecommerce Website Using HTML & CSS"]
+},
+{
+    "Certifications":["HTML","CSS","JAVASCRIPT"],
+    "Source":"GUVI"
+},
+{
+    "Personal Details":["DOB:8/5/00","Age:22","Marital Status:Unmarried","Gender:Male",
+"Languages Known:Tamil,English"]
+},
+{
+    "Declaration":"I here by declare that the above details are true to the best of my knowledge and believes",
+},
+
+]    
+console.log(resume)
+
